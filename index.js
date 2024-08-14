b4473ab5bcab012b51eae129f6ff1be1cea590cd
nimly354 <nobilekimajid@gmail.com>
2024-08-14 19:24:43 +0300

    New repo commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// this line won't be executed
+//function
+//function greeting(){ 
+    /*console.log('good morning')
+    
+}//
+//greeting();
+function sum(){
+    const pi=3.14;
+    var height= 10;
+}
+function findsum( ){
+    var a=5;
+    var b=6;
+    return a + b;
+}
+console.log(findsum())
+  function writearray(){
+var x =[1,2,4,5,7,8];
+    var result=0;
+
+    
+    for( var i=0; i<x.length; i++){
+        console.log(x[i])
+    }
+    
+}
+console.log(writearray()) 
+
+function myfunction(){
+    var number=15
+    if (number<=10){
+    console.log("these is degit")
+}
+    else{
+    console.log("these is a whole number")
+}
+}
+console.log(myfunction())*/l 
+function square(number) {
+    return number * number;
+}
